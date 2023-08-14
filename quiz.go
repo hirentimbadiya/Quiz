ac76cc3f7c2139259eef9d484559907bb5eb5ca8
heyhiru <hirentimbadiya74@gmail.com>
2023-08-14 15:50:32 +0530

    optimized commits
    Signed-off-by: heyhiru <hirentimbadiya74@gmail.com>

--- quiz.go
+++ quiz.go
@@ -15,31 +15,29 @@ func main() {
 	timeLimit := flag.Int("limit", 30, "This is the time limit to give answer in seconds")
 	flag.Parse()
 
-	fmt.Println(time.Duration(*timeLimit) * time.Second)
+	// fmt.Println(time.Duration(*timeLimit) * time.Second)
 	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 
 	file, err := os.Open(*csvFileName)
-	if err != nil {
-		exit(fmt.Sprintf("Failed to Open the CSV file: %s", *csvFileName))
-	}
+	handleErr(err)
 
 	r := csv.NewReader(file)
 	lines, err := r.ReadAll()
 
-	if err != nil {
-		exit("Failed to parse the provided csv file.")
-	}
+	handleErr(err)
 
 	problems := parseLines(lines)
-
 	correctAns := 0
+
 	for i, p := range problems {
 		fmt.Printf("Problem number %d: %s = ", i+1, p.que)
 
+		//* create an answer channel
 		answerCh := make(chan string)
 		go func() {
 			var answer string
 			fmt.Scanf("%s\n", &answer)
+			//* pass the answer to channel whenever user enters answer
 			answerCh <- answer
 		}()
 
@@ -57,22 +55,20 @@ func main() {
 				fmt.Printf("You scored %d out of %d", correctAns, len(problems))
 				return
 			}
-
 		}
 	}
 }
 
 func parseLines(lines [][]string) []problem {
-
-	ret := make([]problem, len(lines))
+	retValue := make([]problem, len(lines))
 
 	for i, line := range lines {
-		ret[i] = problem{
+		retValue[i] = problem{
 			que: line[0],
 			ans: strings.TrimSpace(line[1]),
 		}
 	}
-	return ret
+	return retValue
 }
 
 type problem struct {
@@ -83,4 +79,10 @@ type problem struct {
 func exit(msg string) {
 	fmt.Println(msg)
 	os.Exit(1)
+}
+
+func handleErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
